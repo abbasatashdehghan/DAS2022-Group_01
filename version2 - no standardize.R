fa2c9ab6eb345022fad77c8ab02fd0c336b741df
Chan217 <bigfur16@gmail.com>
2022-03-24 16:55:43 +0800

    Update version2 - no standardize.R

--- version2 - no standardize.R
+++ version2 - no standardize.R
@@ -28,39 +28,60 @@ data$Electricity <- factor(data$Electricity)
 levels(data$Electricity) <- c("No", "Yes")
 glimpse(data)
 
-#======================
+# raw data model, AIC:7015.8
 model_full = glm(data = data, Number_of_Family ~ Income + FoodExpenditure + 
                    Gender + Age + Type + Area + HouseAge + Bedrooms + Electricity,
-                 family = gaussian)
+                 family = poisson)
 summary(model_full)
 outlierTest(model_full)
 
 # drop data
-data <- data[-303,]
-data <- data[-1491,]
 data <- data[-943,]
-data <- data[-1197,]
-data <- data[-1191,]
-data <- data[-230,]
 
-# updated model
+# updated model w/ drop[943] only AIC:6997.4
 model_drop = glm(data = data, Number_of_Family ~ Income + FoodExpenditure + 
                  Gender + Age + Type + Area + HouseAge + Bedrooms + Electricity,
-                 family = gaussian)
+                 family = poisson)
 summary(model_drop)
+outlierTest(model_drop)
 
 tstep <- step(model_drop)
 summary(tstep)
 drop1(tstep)
 
-# remove  Electricity and Area per AIC esult from drop1(tstep) 
-model_drop2 = glm(data = data, Number_of_Family ~ Income + FoodExpenditure + 
-                   Gender + Age + Type + HouseAge + Bedrooms,
-                 family = gaussian)
+# remove Electricity and drop[303] and log Income, Food 
+#AIC:6829.9
+
+data <- data[-303,]
+model_drop2 = glm(data = data, Number_of_Family ~ log(Income) + log(FoodExpenditure) + 
+                  Gender + Age + + Area + Type + HouseAge + Bedrooms,
+                  family = poisson)
 summary(model_drop2)
+outlierTest(model_drop2)
+
+tstep <- step(model_drop)
+summary(tstep)
+drop1(tstep)
+
+# remove  Area per AIC esult from drop1(tstep), drop[1197]
+#AIC:6823.6
+data <- data[-1197,]
+model_drop3 = glm(data = data, Number_of_Family ~ log(Income) + log(FoodExpenditure) + 
+                  Gender + Age + Type + HouseAge + Bedrooms,
+                  family = poisson)
+summary(model_drop3)
+outlierTest(model_drop3)
+
+# remove Age per AIC result from drop1(tstep), drop[1197]
+#AIC:6818.6
+data <- data[-1197,]
+model_drop4 = glm(data = data, Number_of_Family ~ log(Income) + log(FoodExpenditure) + 
+                    Gender + Type + HouseAge + Bedrooms,
+                  family = poisson)
+summary(model_drop4)
+outlierTest(model_drop4)
 # it seems not much difference even dropping variables with low AIC. 
 # Review boxplots to see more 
-
 #============ data recast====================
 p1 <- ggplot(data, aes(x = Number_of_Family, y = Income, fill = Gender)) +
   geom_boxplot() +
